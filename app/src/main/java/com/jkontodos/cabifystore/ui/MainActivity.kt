e19c96ab43f197a558f9092191b28648d8c19d33
Javier Kontos <javierkontos@gmail.com>
2024-05-16 20:45:33 +0200

    The navigation graph and the splash fragment have been created, a custom font has been added to the app, and basic extension functions for views have been implemented

--- app/src/main/java/com/jkontodos/cabifystore/ui/MainActivity.kt
+++ app/src/main/java/com/jkontodos/cabifystore/ui/MainActivity.kt
@@ -3,12 +3,14 @@ package com.jkontodos.cabifystore.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jkontodos.cabifystore.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
